20a550d497f3270199e8574ff7ba04415c496439
connorjohnlind <connorjohnlind@gmail.com>
2018-03-21 13:07:09 -0700

    Added Routing

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import Counter from './components/Counter';
+import { Route, Link } from 'react-router-dom';
 
 import './App.scss';
+import Counter from './components/Counter';
+import Aux from './utils/Aux';
 
-const App = () => <div className="App"><Counter /></div>;
+const Home = () => (
+  <Aux>
+    <Counter />
+    <Link to="/about">About</Link>
+  </Aux>
+);
+
+const About = () => (
+  <div>About Page</div>
+);
+
+const App = () => (
+  <div className="App">
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+  </div>
+);
 
 export default hot(module)(App);
